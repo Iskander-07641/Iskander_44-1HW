31d1a18bf4610ce2379eecba76d9ba91394c3994
Iskander-07641 <iskamagabaku@gmail.com>
2024-08-16 13:03:22 +0600

    3dz

--- database/db.py
+++ database/db.py
@@ -1,10 +1,12 @@
-# database/db.py
-import os
 import sqlite3
+import os
+
 
 class Database:
-    def __init__(self, db_path):
-        os.makedirs(os.path.dirname(db_path), exist_ok=True)
+    def __init__(self, db_name='reviews.db'):
+        db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), db_name)
+        db_path = os.path.abspath(db_path)
+
         self.connection = sqlite3.connect(db_path)
         self.cursor = self.connection.cursor()
         self.create_table()
@@ -13,24 +15,13 @@ class Database:
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS reviews (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                phone_number TEXT NOT NULL,
-                visit_date TEXT NOT NULL,
-                food_rating INTEGER NOT NULL,
-                cleanliness_rating INTEGER NOT NULL,
+                name TEXT,
+                phone_number TEXT,
+                instagram_username TEXT,
+                visit_date TEXT,
+                food_rating INTEGER,
+                cleanliness_rating INTEGER,
                 extra_comments TEXT
             )
         ''')
         self.connection.commit()
-        print("Table created or already exists")  # Отладочное сообщение
-
-    def add_review(self, name, phone_number, visit_date, food_rating, cleanliness_rating, extra_comments):
-        self.cursor.execute('''
-            INSERT INTO reviews (name, phone_number, visit_date, food_rating, cleanliness_rating, extra_comments)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ''', (name, phone_number, visit_date, food_rating, cleanliness_rating, extra_comments))
-        self.connection.commit()
-        print("Review added")  # Отладочное сообщение
-
-    def close(self):
-        self.connection.close()
